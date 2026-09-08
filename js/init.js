5ac1d3a0c5432217a3fc29c1600e9c064e8b6e7b
violette-bleue <purple.asskicker@gmail.com>
2026-09-08 16:43:51 +0200

    wip

--- js/init.js
+++ js/init.js
@@ -68,6 +68,11 @@ function initModules() {
   // fusion toolbar/navbar
   loadModule("./modules/fa-toolbar-merge.js", (m) => m.init());
 
+  // cursor
+  if (document.querySelector("#cursor")) {
+    loadModule("./modules/cursor.js", (m) => m.init());
+  }
+
   // icon navbar
   if (document.querySelector('#modernbb-nav-menu a.mainmenu img, #modernbb-nav-menu a.mainmenu i[icon-mask^="http"]')) {
     loadModule("./modules/nav-icons.js", (m) => m.init());
